b32aa8b4818fbbabc0921c4bec7d57a6bf2570a3
Ikecruz <ikedinobicruz7@gmail.com>
2021-12-17 01:54:06 +0100

    path edit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Header from "./components/header";
+import Header from "./components/header.js";
 import { useState } from "react";
-import Footer from "./components/footer";
+import Footer from "./components/footer.js";
 
 const App = () => {
 
